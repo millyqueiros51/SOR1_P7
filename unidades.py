8ab1583c27ffa94195134f4358ab6b12fe1c0542
Sammily Queirós Osório <99552976+millyqueiros51@users.noreply.github.com>
2022-05-15 14:43:20 -0300

    Add files via upload

---
+++ unidades.py
@@ -0,0 +1,21 @@
+unidades = {
+    'distancia': [
+        'cm',
+        'm',
+        'km'
+    ],
+    'tempo': [
+        's',
+        'min',
+        'h'
+    ],
+    'velocidade': [
+        'm/s',
+        'km/h'
+    ],
+    'temperatura': [
+        'K',
+        '°C',
+        '°F'
+    ]
+}
